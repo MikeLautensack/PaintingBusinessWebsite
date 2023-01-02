288deb5f4d8f851ea6ea6caea586377fe7b2cc41
Mike <mikelautensack100@gmail.com>
2023-01-02 17:51:24 -0500

    first commit

---
+++ src/main/java/com/PaintingBusiness/PaintingBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.PaintingBusiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaintingBusinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PaintingBusiness/PaintingBusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PaintingBusiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaintingBusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
